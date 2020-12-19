f1bf5a923d0436d26368d497ec116ee58810911d
Jakub Brzozowski <jakub.brzozowski.stud@pw.edu.pl>
2020-12-19 20:07:32 +0100

    Dodano obliczenia obciazenia poszczegolnych łączy, Dodano obliczanie funkcji maksymalnego obciazenia lacza

--- parser_test.py
+++ parser_test.py
@@ -34,21 +34,55 @@ linkList, demandList = network_parser.parseXML('net4.xml')
 #    for path in demand.paths:
 #        print(path.id, path.linkIdList)
 
-flow_matrix = {}
+# Alokacja pustej macierzy wymagan
+flowMatrix = {}
 demands = []
 
+# Generacja macierzy wymagan
 for demand in demandList:
     volume = demand.volume
+    # Wyzeruj liste dla nowego wymagania
+    demands = []
+    # Jezeli rozdzielone losowo wymagania nie sumuja sie do calkowitego wymagania to powtorz alokacje wymagan
     while sum(demands) != demand.volume:
+        # Wyzeruj liste dla ponownej alokacji
         demands = []
         for path in demand.paths:
+            # Przydziel losowe wymagania od 0 do calkowitego wymagania jakie zostalo do rozdzielenia
             allocation = random.randint(0, volume - sum(demands))
-            flow_matrix[demand.id, path.id] = allocation
+            # Przypisz wymaganie
+            flowMatrix[demand.id, path.id] = allocation
             demands.append(allocation)
 
+# Wypisz macierz zapotrzebowan
 print("Macierz zapotrzebowan: ")
 for demand in demandList:
     row = []
     for path in demand.paths:
-        row.append(flow_matrix[demand.id, path.id])
+        row.append(flowMatrix[demand.id, path.id])
     print(row)
+
+# Tworzenie pustego slownika obciazen dla laczy
+linkOverload = {}
+for link in linkList:
+    linkOverload[link.id] = 0
+
+# Obliczenie obciazenia laczy
+for demand in demandList:
+    for path in demand.paths:
+        for linkId in path.linkIdList:
+            linkOverload[linkId] += flowMatrix[demand.id, path.id]
+
+print("Lista obciazen dla kazdego lacza: {}".format(linkOverload))
+print("Calkowite obciazenie sieci: {}".format(sum(linkOverload.values())))
+
+# Obliczenie funkcji maksymalnego obciazenia
+for link in linkList:
+    if link.id == 1:
+        # Dla pierwszego lacza przypisz wartosc bez porownywania
+        loadMaximum = linkOverload[link.id] - link.numberOfModules
+    else:
+        # Dla kolejnych lacz wybierz wartosc maksymalna
+        loadMaximum = max(linkOverload[link.id] - link.numberOfModules, loadMaximum)
+
+print("Maksymalna wartosc obciazenia lacza: {}".format(loadMaximum))
