97115ed85a92ad1d864f4bd6e5a24eba53d289cf
steph <stephanie.bulela@gmail.com>
2020-12-21 22:08:49 +0100

    - poprawienie EA - dodanie obsługi kryterium stopu

--- dap_EA.py
+++ dap_EA.py
@@ -23,7 +23,7 @@ import random
 from operator import itemgetter
 
 # Parse network file
-linkList, demandList = network_parser.parseXML('net4.xml')
+linkList, demandList = network_parser.parseXML('net12_1.xml')
 
 
 class Chromosome:
@@ -83,20 +83,6 @@ class Chromosome:
             else:
                 # Dla kolejnych lacz wybierz wartosc maksymalna
                 self.loadMaximum = max(self.linkLoad[link.id] - link.numberOfModules*link.linkModule, self.loadMaximum)
-        #print("Load Maximum {}".format(self.loadMaximum))
-
-
-    def getLinkLoad(self):
-        return self.linkLoad
-
-    def getFitnessFunction (self):
-        return self.loadMaximum
-
-    def calculateFitnessFunction(self):
-        return 0
-
-    def modifyGene(self):
-        return 0
 
 
 """ Algorytm Ewolucyjny
@@ -110,154 +96,204 @@ class Chromosome:
 
 current_population = []
 population_size = 6
+#lista wszystkich populacji (generacji)
 populationList = []
 
-#najlepsze chormosomy z populacji
+#najlepsze chormosomy z populacji, które zostają rodzicami
 parents = []
-number_of_parents = 4
+number_of_parents = 2
+
+#Crossover + Mutacja
+pstwo_crossover = 0.5
+pstwo_mutation = 0.5
 
 i = 0
 #1.Generujemy pierwszą populację
 while i < population_size:
     newChromosome = Chromosome(linkList, demandList)
     current_population.append(newChromosome)
-    #print(population[i].geneList)
-    #print(population[i].flowMatrix)
-    #print(population[i])
-    #print(population[i])
     i += 1
 
-populationList.append(current_population)
-print()
+populationList.append(deepcopy(current_population))
 
-#Wybieramy najlepsze chormosomy z populacji - czyli takie, które mają namniejszą wartość loadMaximum
+#2.Wybieramy najlepsze chormosomy z populacji - czyli takie, które mają namniejszą wartość loadMaximum
 
-#najpierw sortujemy chormosomy w populacji po loadMaximum
-population_sorted = []
-
-
+#najpierw sortujemy chormosomy w populacji po loadMaximum w pierwszej generacji
 def funcSortChormosomes(e):
   return e.loadMaximum
 
 current_population.sort(key=funcSortChormosomes)
-#population_sorted = sorted(population, key=funcSortChormosomes)
 
-print("\nPopulacja (chromosomy):")
+'''
+print("\nBieżąca populacja (chromosomy):")
 i = 0
 while i < population_size:
     print(current_population[i], " ", current_population[i].loadMaximum)
     i += 1
 print()
+'''
+#Kryterium stopu
+max_iteration = 150
+max_generation = 150
+max_mutation = 800
+max_imprv_count = 30
 
-"""i = 0
-while i < population_size:
-    print(population_sorted[i]," ", population_sorted[i].loadMaximum)
-    i += 1"""
+number_of_mutation = 0
+imprv = False
+impr_count = 0
 
-#Wybieramy rodziców
+it = 0
+while it < max_iteration and len(populationList) < max_generation and number_of_mutation < max_mutation and impr_count < max_imprv_count:
+#Wybieramy rodziców - 4 pierwsze chromosomy w posortowanej populacji
+    parents.clear()
+    for i in range(0, number_of_parents):
+        newParent = deepcopy(current_population[i])
+        parents.append(newParent)
 
-for i in range(0, number_of_parents):
-    newParent = deepcopy(current_population[i])
-    parents.append(newParent)
-i = 0
+    i = 0
+    '''
+    print("\nParents (4 najlepsze chormosomy):")
+    while i < number_of_parents:
+        print(parents[i]," ", parents[i].loadMaximum)
+        i += 1
+    '''
 
-print("\nParents (4 najlepsze chormosomy):")
-while i < number_of_parents:
-    print(parents[i]," ", parents[i].loadMaximum)
-    i += 1
+    #if random.uniform(0, 1)) < pstwo_crossover:
 
+    #print(len(demandList)/2)
+    offSpringList = []
 
-#Crossover + Mutacja
-pstwo_crossover = 0.5
-pstwo_mutation = 0.5
-#if random.uniform(0, 1)) < pstwo_crossover:
+    i = 0
+    j = 0
+    #Generowanie potomstwa:
+    # Crossover - połowa genów jedengo rodzica i połowa genów drugiego rodzica
+    # Mutacja - zmieniamy obciążenie dwóch losowych ścieżek w losowym żądaniu
 
-print(len(demandList)/2)
-offSpringList = []
+    while i < number_of_parents:
+        newFlowMatrix = {}
 
-i = 0
-j = 0
-#Generowanie potomstwa: Crossover - połowa genów jedengo rodzica i połowa genów drugiego rodzica
-while i < number_of_parents:
-    newFlowMatrix = {}
-    for demand in demandList[0: int(len(demandList)/2)]:
-        for path in demand.paths:
-            newFlowMatrix[demand.id, path.id] = parents[i].flowMatrix[demand.id, path.id]
+        for demand in demandList:
+            if random.uniform(0, 1) > pstwo_crossover:
+                for path in demand.paths:
+                    newFlowMatrix[demand.id, path.id] = parents[i].flowMatrix[demand.id, path.id]
+            else:
+                for path in demand.paths:
+                    newFlowMatrix[demand.id, path.id] = parents[i + 1].flowMatrix[demand.id, path.id]
 
-    for demand in demandList[int(len(demandList)/2):int(len(demandList))]:
-        for path in demand.paths:
-            newFlowMatrix[demand.id, path.id] = parents[i+1].flowMatrix[demand.id, path.id]
-    i += 2
-    j += 1
+        i += 2
+        j += 1
 
-    offSpring = Chromosome(linkList, demandList)
-    offSpring.flowMatrix = newFlowMatrix
+        offSpring = Chromosome(linkList, demandList)
+        offSpring.flowMatrix = newFlowMatrix
 
-    print("\nRodzic {}".format(i-2))
-    print(parents[i-2].flowMatrix)
-    print("\nRodzic {}".format(i-1))
-    print(parents[i-1].flowMatrix)
-    print("\nPotomstwo {}".format(j))
-    print(offSpring.flowMatrix)
-
-#Generowanie potomastwa - mutacja - zmieniamy obciążenie dwóch losowych ścieżek w losowym żądaniu
-    if random.uniform(0, 1) > pstwo_mutation:
-        # losowe żądanie
-        randomDemandId = random.randint(0, len(demandList)-1) + 1
-        # losowe ścieżki
-        pathLen = len(demandList[randomDemandId-1].paths)
-        pathRandomOne = random.randint(1, pathLen)
-        pathRandomTwo = pathRandomOne
-        while pathRandomTwo == pathRandomOne:
-            pathRandomTwo = random.randint(1, pathLen)
-
-        pathLoadValueOne = offSpring.flowMatrix[randomDemandId, pathRandomOne]
-        pathLoadValueTwo = offSpring.flowMatrix[randomDemandId, pathRandomTwo]
-
-        #zamiana
-        offSpring.flowMatrix[randomDemandId, pathRandomOne] = pathLoadValueTwo
-        offSpring.flowMatrix[randomDemandId, pathRandomTwo] = pathLoadValueOne
-
-        print("Potomstwo {} po mutacji genu {}, zamiana ścieżek {} i {}".format(j, randomDemandId,pathRandomOne, pathRandomTwo))
+        """
+        print("\nRodzic {}".format(i-2))
+        print(parents[i-2].flowMatrix)
+        print("\nRodzic {}".format(i-1))
+        print(parents[i-1].flowMatrix)
+        print("\nPotomstwo {}".format(j))
         print(offSpring.flowMatrix)
-    offSpringList.append(offSpring)
+        """
 
-print("Old LoadMax")
-for i in range(0, j):
-    print(current_population[i].loadMaximum)
+        #Mutacja - zmieniamy obciążenie dwóch losowych ścieżek w losowym żądaniu
+        if random.uniform(0, 1) > pstwo_mutation:
+            number_of_mutation += 1
+            # losowe żądanie
+            randomDemandId = random.randint(0, len(demandList)-1) + 1
+            # losowe ścieżki
+            pathLen = len(demandList[randomDemandId-1].paths)
+            pathRandomOne = random.randint(1, pathLen)
+            pathRandomTwo = pathRandomOne
 
-#Ewaluacja
-for offSpring in offSpringList:
-    offSpring.setLinkLoad()
-    offSpring.sefLoadMax()
+            # jeżeli dane zapotrzebowanie ma tylko jedną ścieżkę to nie da się przeprowadzić mutacji
+            if pathLen == 1:
+                break
+            while pathRandomTwo == pathRandomOne:
+                pathRandomTwo = random.randint(1, pathLen)
 
-print("New LoadMax")
-for offSpring in offSpringList:
-    print(offSpring.loadMaximum)
+            pathLoadValueOne = offSpring.flowMatrix[randomDemandId, pathRandomOne]
+            pathLoadValueTwo = offSpring.flowMatrix[randomDemandId, pathRandomTwo]
 
-#Tworzenie nowej populacji - wybieramy najlepszych osobników
+            #zamiana
+            offSpring.flowMatrix[randomDemandId, pathRandomOne] = pathLoadValueTwo
+            offSpring.flowMatrix[randomDemandId, pathRandomTwo] = pathLoadValueOne
 
-# tworzymy wspólną listę wszytskich chormosomów
-newChromosomeList = current_population + offSpringList
+            #print("Potomstwo {} po mutacji genu {}, zamiana ścieżek {} i {}".format(j, randomDemandId,pathRandomOne, pathRandomTwo))
+            #print(offSpring.flowMatrix)
+        offSpringList.append(offSpring)
 
-print(newChromosomeList)
+    '''
+    print("\nOld LoadMax")
+    for i in range(0, j):
+        print(current_population[i].loadMaximum)
+    '''
 
-print("Nieposortowana połaczona lista chormosomów starych i potomków")
-for chromosome in newChromosomeList:
-    print(chromosome.loadMaximum)
+    #Ewaluacja
+    for offSpring in offSpringList:
+        offSpring.setLinkLoad()
+        offSpring.sefLoadMax()
 
-#sotrujemy listę wszytskich chromosomów
-newChromosomeList.sort(key=funcSortChormosomes)
+    '''
+    print("New LoadMax")
+    for offSpring in offSpringList:
+        print(offSpring.loadMaximum)
+    '''
 
-print("Posortowana połaczona lista chormosomów starych i potomków")
-for chromosome in newChromosomeList:
-    print(chromosome.loadMaximum)
+    #4. Tworzenie nowej populacji - wybieramy najlepszych osobników
 
-# wybieramy najlepszych
-current_population = newChromosomeList[0:population_size]
-populationList.append(current_population)
+    # tworzymy wspólną listę wszytskich chormosomów starych i nowych
+    newChromosomeList = current_population + offSpringList
 
-print("New Population")
-for chromosome in current_population:
-    print(chromosome.loadMaximum)
+    """
+    print("Nieposortowana połaczona lista chormosomów starych i potomków")
+    for chromosome in newChromosomeList:
+        print(chromosome.loadMaximum)
+    """
 
+    # Sotrujemy listę wszytskich chromosomów
+    newChromosomeList.sort(key=funcSortChormosomes)
+
+    """
+    print("Posortowana połaczona lista chormosomów starych i potomków")
+    for chromosome in newChromosomeList:
+        print(chromosome.loadMaximum)
+    """
+
+    # Tworzymy nową generację wybierając najlepszych
+    current_population.clear()
+    current_population = newChromosomeList[0:population_size]
+    populationList.append(deepcopy(current_population))
+
+    '''
+    print("New Population {}".format(len(populationList)))
+    for chromosome in current_population:
+        print(chromosome.loadMaximum)
+    '''
+    it += 1
+
+
+    if populationList[-1][0].loadMaximum < populationList[-2][0].loadMaximum:
+        imprv = True
+        impr_count = 0
+    else:
+        imprv = False
+        impr_count += 1
+
+
+print()
+print("Liczba iteracji: {}".format(it))
+print("Liczba populacji: {}".format(len(populationList)))
+print("Liczba mutacji: {}".format(number_of_mutation))
+print("Liczba populacji bez poprawy: {}".format(impr_count))
+stop_reason = ""
+if it >= max_iteration:
+    stop_reason = "Przekroczono dopuszczalną liczbę iteracji"
+elif len(populationList) >= max_generation:
+    stop_reason = "Przekroczono dopuszczalną liczbę genracji"
+elif number_of_mutation >= max_mutation:
+    stop_reason = "Przekroczono dopuszczalną liczbę mutacji"
+elif impr_count >= max_imprv_count:
+    stop_reason = "Przekroczono dopuszczalną liczbę genracji bez poprawy"
+
+print("Kryterium stopu: {}".format(stop_reason))
+print("Najlepsze rozwiązanie minF = {}".format(populationList[-1][0].loadMaximum))
